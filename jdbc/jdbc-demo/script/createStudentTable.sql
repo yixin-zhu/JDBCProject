b04cde196b6126843a8b75225d02fded1f8d388a
fudan <zhu_yixin@foxmail.com>
2022-10-23 11:59:36 +0800

    lab1 finish

--- jdbc/jdbc-demo/script/createStudentTable.sql
+++ jdbc/jdbc-demo/script/createStudentTable.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `lab1demo`.`student` ( `registno` VARCHAR(50) NULL DEFAULT NULL ,
+CREATE TABLE `lab1demo`.`student`
+( `registno` VARCHAR(50) NULL DEFAULT NULL ,
 `name` VARCHAR(50) NULL DEFAULT NULL ,
 `kdno` INT NULL DEFAULT NULL ,
 `kcno` INT NULL DEFAULT NULL ,
--- jdbc/jdbc-demo/src/com/database/jdbc/JDBCDemo.java
+++ jdbc/jdbc-demo/src/com/database/jdbc/JDBCDemo.java
@@ -1,51 +1,153 @@
 package com.database.jdbc;
 
-import com.mysql.cj.protocol.Resultset;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.opencsv.CSVReader;
 
+import javax.sql.DataSource;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 
 public class JDBCDemo {
+    static Properties prop;
+    static DataSource dataSource;
+    static Connection conn;
+
+    static final String fileName1 = "jdbc-demo/data/room.csv";
+    static final String fileName2 = "jdbc-demo/data/student.csv";
+    static final String tableName1 = "room";
+    static final String tableName2 = "student";
 
     public static void main(String[] args) throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        final String URL = "jdbc:mysql://localhost:3306/lab1demo?useServerPrepStmts=true";
-        final String USER = "root";
-        final String PASSWORD = "123";
-        Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-
-        String sql1 = "update room set papername = 'B' where kdname = 'kkk' ";
-        String sql2 = "select * from room where papername = ?";
-
-        Statement stmt = conn.createStatement();
-        PreparedStatement pstmt = conn.prepareStatement(sql2);
-
-        pstmt.setString(1, "D");
-
-        // int count = stmt.executeUpdate(sql1);
-        ResultSet rs = pstmt.executeQuery();
-
-        // System.out.println("result is " + count);
-        while(rs.next()){
-            int kdno = rs.getInt("kdno");
-            String kdname = rs.getString("kdname");
-            Timestamp exptime = rs.getTimestamp("exptime");
-            System.out.println("result is " + kdno);
-            System.out.println("result is " + kdname);
-            System.out.println("result is " + exptime);
-        }
-
-
-        rs.close();
-        pstmt.close();
+        // prepare environment until connection ready
+        prepareAndConnect();
+        System.out.println("Connect established. The connection is " + conn);
+        runScript("jdbc-demo/script/createRoomTable.sql");
+        runScript("jdbc-demo/script/createStudentTable.sql");
+        System.out.println( "insert into " + tableName1 + " " +
+                insert(fileName1, tableName1) + " rows have been affected"
+        );
+        System.out.println( "insert into" + tableName2 + " " +
+                insert(fileName2, tableName2) + " rows have been affected"
+        );
         conn.close();
-
-
-
-
-
     }
 
+    private static void runScript(String scriptName) throws Exception {
+        String allCommands = new String(Files.readAllBytes(Paths.get(scriptName)));
+        String[] commands = allCommands.split(";");
+        Statement stmt = conn.createStatement();
+        for(String command : commands){
+            stmt.execute(command);
+        }
+    }
 
+    private static List<String[]> readAllFromCSV(String file) throws Exception {
+        FileReader filereader = new FileReader(file);
+        CSVReader csvReader = new CSVReader(filereader);
+        List<String[]> allData = csvReader.readAll();
+        /*for (String[] row : allData) {
+            for (String cell : row) {
+                System.out.print(cell + "\t");
+            }
+            System.out.println();
+        }*/
+        return allData;
+    }
+
+    private static int insert(String fileName, String tableName) throws Exception{
+        int count = 0;
+        List<String[]> allData = readAllFromCSV(fileName);
+        List<String> allDataType = tableFieldTypes(tableName);
+        int m = allData.size()-1;
+        String s1 = getParameterSign(allData.get(0));
+        String s2 = getValueSign(allData.get(0).length);
+        for(int i=1; i<m; i++){
+            String sql = String.format("insert into %s(%s) values (%s)", tableName, s1, s2);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            for(int j =0; j<allData.get(0).length; j++){
+                if(noValue(allDataType.get(j))){
+                    pstmt.setString(j+1, "null");
+                } else {
+                    pstmt.setString(j+1, allData.get(i)[j]);
+                }
+            }
+            count += pstmt.executeUpdate();
+            pstmt.close();
+        }
+        return count;
+    }
+
+    private static boolean noValue(String s) {
+        return s== null || s.length() == 0;
+    }
+
+    private static List<String> tableFieldTypes(String tableName) throws Exception {
+        String sql = String.format("select * from %s", tableName);
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery(sql);
+        return getFieldTypes(rs);
+    }
+
+    private static String getParameterSign(String[] types) {
+        int m = types.length;
+        String ans = new String();
+        for(int i=0;i<m-1;i++){
+            ans = ans.concat(types[i] + ",");
+        }
+        ans = ans.concat(types[m-1]);
+        return ans;
+    }
+
+    private static String getValueSign(int m) {
+        String ans = new String();
+        for(int i=0;i<m-1;i++){
+            ans = ans.concat("?,");
+        }
+        ans = ans.concat("?");
+        return ans;
+    }
+
+    private static boolean needQuote(String s) {
+        boolean flag = false;
+        String[] needTypes = {"CHAR", "VARCHAR","TEXT", "DATE", "DATETIME", "TIMESTAMP", "YEAR", "TIME"};
+        for( String type : needTypes){
+            if(s.equalsIgnoreCase(type)){
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    private static void prepareAndConnect() throws Exception {
+        prop = new Properties();
+        prop.load(new FileReader("jdbc-demo/src/druid.properties"));
+        dataSource = DruidDataSourceFactory.createDataSource(prop);
+        conn = dataSource.getConnection();
+    }
+
+    private static List<String> getFields(ResultSet rs) throws Exception {
+        List<String> fields = new ArrayList<>();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        for(int i=1; i<=rsmd.getColumnCount();i++){
+            fields.add(rsmd.getColumnName(i));
+        }
+        return fields;
+    }
+
+    private static List<String> getFieldTypes(ResultSet rs) throws Exception {
+        List<String> fieldTypes = new ArrayList<>();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        for(int i=1; i<=rsmd.getColumnCount();i++){
+            fieldTypes.add(rsmd.getColumnTypeName(i));
+        }
+        return fieldTypes;
+    }
 
 }
